724dd251d4050e80cb6e0772bf1bf80325c44fa4
William Chelman <william.chelman@cogni.zone>
2021-07-18 20:13:16 +0200

    chore: added metadaat to package files

---
+++ tools/sync-package-metadata.js
@@ -0,0 +1,26 @@
+const { readdirSync, readFileSync, writeFileSync } = require('fs');
+const { join, dirname, relative } = require('path');
+
+const libDist = join(__dirname, '../libs');
+const packageFiles = readdirSync(libDist).map(dir => join(libDist, dir, 'package.json'));
+
+function readJson(path) {
+  return JSON.parse(readFileSync(path, 'utf8'));
+}
+
+function writeJson(path, content) {
+  writeFileSync(path, JSON.stringify(content, null, 2));
+}
+
+const rootPackage = readJson(join(__dirname, '../package.json'));
+
+packageFiles.forEach(packageFile => {
+  console.log('Syncing', packageFile);
+  const package = readJson(packageFile);
+  const directory = relative(join(__dirname, '..'), dirname(packageFile)).replace(/\\/g, '/');
+  package.license = rootPackage.license;
+  package.homepage = rootPackage.homepage;
+  package.contributors = rootPackage.contributors;
+  package.repository = { ...rootPackage.repository, directory };
+  writeJson(packageFile, package);
+});
