909d4f30e47a920a54eb410a6b82ab0dd7940535
William Chelman <william.chelman@cogni.zone>
2021-07-26 12:35:59 +0200

    chore: release scripts

---
+++ release.config.js
@@ -0,0 +1,42 @@
+const { readdirSync } = require('fs');
+const { join } = require('path');
+
+const libDist = join(__dirname, 'dist/libs');
+const packagesRoots = readdirSync(libDist).map(dir => join(libDist, dir));
+
+const npmPlugins = packagesRoots.map(pkgRoot => [
+  '@semantic-release/npm',
+  {
+    pkgRoot
+  }
+]);
+
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'rc', prerelease: true },
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ...npmPlugins,
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'node ./tools/sync-versions.js'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'libs/**/package.json']
+      }
+    ]
+  ]
+};
---
+++ tools/sync-versions.js
@@ -0,0 +1,14 @@
+const { readdirSync, readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+
+const libDist = join(__dirname, '../dist/libs');
+const distPackageJsonFiles = readdirSync(libDist).map(dir => join(libDist, dir, 'package.json'));
+
+distPackageJsonFiles.forEach(distPath => {
+  const dist = JSON.parse(readFileSync(distPath));
+  const srcPath = distPath.replace('/dist/', '/');
+  const src = JSON.parse(readFileSync(srcPath));
+  src.version = dist.version;
+  console('Syncing version between dist and src package files', distPath, srcPath, dist.version);
+  writeFileSync(srcPath, JSON.stringify(src, null, 2), 'utf8');
+});
