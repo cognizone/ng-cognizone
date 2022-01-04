428a85b219e110cd1ab9a73bcdfca343251029fa
William Chelman <william.chelman@cogni.zone>
2022-01-04 14:01:58 +0100

    fix(model-utils): remove "first lang found" fallback from czLabelToString

--- libs/model-utils/src/lib/models/cz-label.ts
+++ libs/model-utils/src/lib/models/cz-label.ts
@@ -1,4 +1,5 @@
-import { getLangStringValue, LangString, LangStringSimple } from './lang-string';
+import { LangString, LangStringSimple } from './lang-string';
+import { manyToOne } from './many';
 import { Nil } from './nil';
 
 /**
@@ -7,10 +8,23 @@ import { Nil } from './nil';
 export type CzLabel = LangString | LangStringSimple | string;
 
 /**
- * same as {@link getLangStringValue} but also return label as-is if it's already a string
+ * Extract from a {@link CzLabel} the label in a corresponding `lang`. If not
+ * found, it will try to extract the label of one of the fallback languages.
+ * It is to be noted that even if the label is a string[] with multiple values,
+ * only the 0th element will be taken.
+ *
+ * @param langString The lang string from which we want to extract a label
+ * @param lang The preferred lang to get the label for
+ * @param fallbackLangs ordered list of fallback languages
  */
 export function czLabelToString(label: Nil<CzLabel>, lang: string, fallbackLangs: string[] = []): string | undefined {
   if (label == null) return undefined;
   if (typeof label === 'string') return label;
-  return getLangStringValue(label, lang, fallbackLangs);
+
+  const allLangs = [lang, ...fallbackLangs];
+
+  for (const language of allLangs) {
+    const value = manyToOne(label[language]);
+    if (value) return value;
+  }
 }
