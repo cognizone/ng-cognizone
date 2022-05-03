dfbfb60b34b4ce7f75b7dfca9f934867743b186c
William Chelman <william.chelman@cogni.zone>
2022-05-03 16:01:23 +0200

    feat(json-model): smarter default IdGenerator

--- libs/json-model/src/lib/services/id-generator.service.ts
+++ libs/json-model/src/lib/services/id-generator.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Many, manyToArray } from '@cognizone/model-utils';
 
-// TODO replace with http://resource generation for default
-
 @Injectable()
 export class IdGenerator {
+  prefix: string = 'http://resource/';
+
   private counter = 0;
 
   generateId(types: Many<string>): string {
-    const prefix = 'http://resource';
     types = manyToArray(types).map(encodeURIComponent).join('-');
-    return `${prefix}/${types}/${this.counter++}-${Date.now()}`;
+    let prefix = this.prefix;
+    if (!prefix.endsWith('#') && !prefix.endsWith('/')) {
+      prefix = `${prefix}/`;
+    }
+    return `${prefix}${types}/${this.counter++}-${Date.now()}`;
   }
 }
