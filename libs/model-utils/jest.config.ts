52845a0d5534366a664050c6867fbe4cfba93c48
William Chelman <william.chelman@cogni.zone>
2022-11-14 17:41:47 +0100

    chore(model-utils): build as pure typescript library

--- libs/model-utils/jest.config.ts
+++ libs/model-utils/jest.config.ts
@@ -2,22 +2,15 @@
 export default {
   displayName: 'model-utils',
   preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
-      stringifyContentPathRegex: '\\.(html|svg)$',
-
       tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
-  coverageDirectory: '../../coverage/libs/model-utils',
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
+  testEnvironment: 'jsdom',
   transform: {
-    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.[tj]s$': 'ts-jest',
   },
-  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/model-utils',
 };
