4d24cbc48af55e3f67be2409ea6142d988b27121
William Chelman <william.chelman@cogni.zone>
2023-03-15 17:35:44 +0100

    chore: upgrade node and npm versions

--- apps/docs-app/src/app/shared/shared.module.ts
+++ apps/docs-app/src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -69,8 +68,8 @@ const directives = [MatTabRoutedDirective, AnchorDirective, ImgTooltipDirective,
 
 @NgModule({
     declarations: [components, pipes, directives],
-    imports: [CommonModule, material, ReactiveFormsModule, FlexLayoutModule, RouterModule],
-    exports: [material, components, pipes, directives, CommonModule, ReactiveFormsModule, FlexLayoutModule],
+    imports: [CommonModule, material, ReactiveFormsModule, RouterModule],
+    exports: [material, components, pipes, directives, CommonModule, ReactiveFormsModule],
     providers: [
         {
             provide: TOOLTIP_COMPONENT,
