0194b71088ec3b422301b04ab56e3a0c358f23b7
William Chelman <william.chelman@cogni.zone>
2023-04-27 09:51:09 +0200

    feat(ng-core): added LoadingServiceFactory to ease reuse with defined options

--- libs/ng-core/src/lib/services/loading.service.ts
+++ libs/ng-core/src/lib/services/loading.service.ts
@@ -1,12 +1,21 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { inject, Injectable, InjectionToken, OnDestroy, Injector } from '@angular/core';
 import { BehaviorSubject, MonoTypeOperatorFunction, Observable, pipe } from 'rxjs';
 import { distinctUntilChanged, finalize, map } from 'rxjs/operators';
 
 import { startWithTap } from '../operators/start-with-tap';
 
+export const DEFAULT_LOADING_SERVICE_OPTIONS: LoadingServiceOptions = {
+  defaultKey: '_root',
+};
+
+export const LOADING_SERVICE_OPTIONS = new InjectionToken<LoadingServiceOptions>('LOADING_SERVICE_OPTIONS', {
+  providedIn: 'root',
+  factory: () => DEFAULT_LOADING_SERVICE_OPTIONS,
+});
+
 @Injectable({ providedIn: 'root' })
 export class LoadingService implements OnDestroy {
-  readonly defaultKey = '_root';
+  private options = inject(LOADING_SERVICE_OPTIONS);
 
   loading$: Observable<boolean>;
 
@@ -21,29 +30,29 @@ export class LoadingService implements OnDestroy {
     this.loading$ = this.getLoading();
   }
 
-  addLoading(key: string = this.defaultKey): void {
+  addLoading(key: string = this.options.defaultKey): void {
     this.addLoadingInternal(key, 1);
   }
 
-  removeLoading(key: string = this.defaultKey): void {
+  removeLoading(key: string = this.options.defaultKey): void {
     this.addLoadingInternal(key, -1);
   }
 
-  asOperator<T>(key: string = this.defaultKey): MonoTypeOperatorFunction<T> {
+  asOperator<T>(key: string = this.options.defaultKey): MonoTypeOperatorFunction<T> {
     return pipe(
       startWithTap(() => this.addLoading(key)),
       finalize(() => this.removeLoading(key))
     );
   }
 
-  getLoading(key: string = this.defaultKey): Observable<boolean> {
+  getLoading(key: string = this.options.defaultKey): Observable<boolean> {
     return this._loadings$.asObservable().pipe(
       map(loadings => this.getLoadingSnapshotInternal(loadings, key)),
       distinctUntilChanged()
     );
   }
 
-  getLoadingSnapshot(key: string = this.defaultKey): boolean {
+  getLoadingSnapshot(key: string = this.options.defaultKey): boolean {
     return this.getLoadingSnapshotInternal(this._loadings$.value, key);
   }
 
@@ -64,6 +73,26 @@ export class LoadingService implements OnDestroy {
   }
 }
 
+@Injectable({ providedIn: 'root' })
+export class LoadingServiceFactory {
+  private injector = inject(Injector);
+
+  create(options: LoadingServiceOptions): LoadingService {
+    const newInjector = Injector.create({
+      providers: [
+        { provide: LOADING_SERVICE_OPTIONS, useValue: options },
+        { provide: LoadingService, useClass: LoadingService, deps: [LOADING_SERVICE_OPTIONS] },
+      ],
+      parent: this.injector,
+    });
+    return newInjector.get(LoadingService);
+  }
+}
+
 interface Loadings {
   [key: string]: number;
+}
+
+export interface LoadingServiceOptions {
+  defaultKey: string;
 }
