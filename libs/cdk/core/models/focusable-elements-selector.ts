8498673c7e68b5171c7d9b13803b4e457f400cce
William Chelman <william.chelman@cogni.zone>
2023-11-07 15:21:13 +0100

    feat(cdk): added AutoResizeHelperDirective and FOCUSABLE_ELEMENTS_SELECTOR

---
+++ libs/cdk/core/models/focusable-elements-selector.ts
@@ -0,0 +1,15 @@
+export const FOCUSABLE_ELEMENTS_SELECTOR = [
+  'a[href]',
+  'area[href]',
+  'button:not([disabled])',
+  'embed',
+  'iframe',
+  'input:not([disabled])',
+  'object',
+  'select:not([disabled])',
+  'textarea:not([disabled])',
+  '*[tabindex]:not([aria-disabled]):not([tabindex="-1"])',
+  '*[contenteditable]',
+  'details:not([disabled])',
+  'summary:not(:disabled)',
+].join(',');
