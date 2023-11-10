e8ac70198ccc88906028e7157ea580f68a88f07f
William Chelman <william.chelman@cogni.zone>
2023-11-10 13:46:08 +0100

    chore(lod): moved KNOWN_PREFIXES to lod

--- libs/cli/src/models/default-prefixes.ts
+++ libs/cli/src/models/default-prefixes.ts
@@ -1,3 +1,10 @@
-import { KNOWN_PREFIXES, Prefixes } from '@cognizone/model-utils';
+export interface Prefixes {
+  [prefix: string]: string;
+}
 
-export const DEFAULT_PREFIXES: Prefixes = { ...KNOWN_PREFIXES, foaf: 'http://xmlns.com/foaf/0.1/' };
+export const DEFAULT_PREFIXES: Prefixes = {
+  xsd: 'http://www.w3.org/2001/XMLSchema#',
+  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+  rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+  foaf: 'http://xmlns.com/foaf/0.1/',
+};
--- libs/model-utils/src/lib/models/index.ts
+++ libs/model-utils/src/lib/models/index.ts
@@ -8,7 +8,6 @@ export * from './lang-string';
 export * from './many';
 export * from './nil';
 export * from './pagination';
-export * from './prefixes';
 export * from './primitive';
 export * from './select-option';
 export * from './sorter';
