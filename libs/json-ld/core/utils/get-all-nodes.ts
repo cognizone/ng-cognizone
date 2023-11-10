2551b8558ce73de04636eb8d3460be7ee2f758c5
William Chelman <william.chelman@cogni.zone>
2023-11-10 13:22:54 +0100

    feat(shacl): added shacl lib

--- libs/json-ld/core/utils/get-all-nodes.ts
+++ libs/json-ld/core/utils/get-all-nodes.ts
@@ -1,12 +1,12 @@
-import { ExpandedJsonLdContainer, JsonLdNode, JsonLdNodeFlat, JsonLdValue } from '../models';
+import { ExpandedJsonLdContainer, JsonLdNode, JsonLdValue } from '../models';
 import { getAllValuesGen } from './get-all-values';
 
 export function* getAllNodes<T extends JsonLdNode = JsonLdNode>(
   values: JsonLdValue[] | undefined,
   graph: ExpandedJsonLdContainer
-): Generator<JsonLdNodeFlat<T>> {
+): Generator<T> {
   for (const uri of getAllValuesGen<string>(values, graph)) {
     if (typeof uri !== 'string' || !graph.nodes[uri]) continue;
-    yield graph.nodes[uri] as JsonLdNodeFlat<T>;
+    yield graph.nodes[uri] as T;
   }
 }
---
+++ libs/shacl/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/models';
+export * from './lib/services';
+export * from './lib/utils';
