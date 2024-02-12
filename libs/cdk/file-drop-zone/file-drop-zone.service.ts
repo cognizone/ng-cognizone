9c78a84631b1fc0884c6e79b7c6b50cd3e3054bb
William Chelman <william.chelman@cogni.zone>
2024-02-12 17:21:57 +0100

    feat(cdk): added file-drop-zone directive and service

---
+++ libs/cdk/file-drop-zone/file-drop-zone.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NgZone, inject } from '@angular/core';
+import { SubSink, notNil } from '@cognizone/model-utils';
+import { fromEvent, merge, filter } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FileDropZoneService {
+  private ngZone: NgZone = inject(NgZone);
+  private subSink: SubSink = new SubSink();
+  private registered: Set<unknown> = new Set();
+
+  registerDropZone(obj: unknown): void {
+    if (this.registered.size === 0) {
+      this.preventDefaultDragAndDrop();
+    }
+    this.registered.add(obj);
+  }
+
+  unregisterDropZone(obj: unknown): void {
+    this.registered.delete(obj);
+    if (this.registered.size === 0) {
+      this.subSink.empty();
+    }
+  }
+
+  // without this, the browser tries to open the file in the current tab
+  private preventDefaultDragAndDrop(): void {
+    this.ngZone.runOutsideAngular(() => {
+      const dragover$ = fromEvent(window, 'dragover');
+      const drop$ = fromEvent(window, 'drop');
+      this.subSink.add = merge(drop$, dragover$)
+        .pipe(filter(notNil))
+        .subscribe(event => {
+          event.preventDefault();
+        });
+    });
+  }
+}
---
+++ libs/cdk/file-drop-zone/index.ts
@@ -0,0 +1,2 @@
+export * from './file-drop-zone.directive';
+export * from './file-drop-zone.service';
