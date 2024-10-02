171e345ec5c2631da67fd52cafc3bf53dff5591e
William Chelman <WilliamChelman@users.noreply.github.com>
2024-10-02 17:58:15 +0200

    fix(cdk/result): better typing for pipes

--- libs/cdk/result/pipes/get-success.pipe.ts
+++ libs/cdk/result/pipes/get-success.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Result } from '@cognizone/model-utils';
+import { AsyncResult } from '@cognizone/model-utils';
 
 @Pipe({
   name: 'getSuccess',
   standalone: true,
 })
 export class GetSuccessPipe implements PipeTransform {
-  transform<T>(value: Result<T>): T | undefined {
+  transform<T>(value: AsyncResult<T>): T | undefined {
     return value.type === 'success' ? value.content : undefined;
   }
 }
--- libs/cdk/result/pipes/is-error.pipe.ts
+++ libs/cdk/result/pipes/is-error.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Result, ResultError } from '@cognizone/model-utils';
+import { AsyncResult, ResultError } from '@cognizone/model-utils';
 
 @Pipe({
   name: 'isError',
   standalone: true,
 })
 export class IsErrorPipe implements PipeTransform {
-  transform(value: Result): value is ResultError<unknown> {
+  transform(value: AsyncResult): value is ResultError<unknown> {
     return value.type === 'error';
   }
 }
