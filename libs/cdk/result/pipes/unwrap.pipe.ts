6bed0277066e17c3cc099b8b0445ad0498e2acd6
William Chelman <WilliamChelman@users.noreply.github.com>
2024-12-05 16:45:09 +0100

    fix(cdk): handle nil values in unwrap pipe

--- libs/cdk/result/pipes/unwrap.pipe.ts
+++ libs/cdk/result/pipes/unwrap.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Result } from '@cognizone/model-utils';
+import { Nil, Result } from '@cognizone/model-utils';
 
 @Pipe({
   name: 'unwrap',
   standalone: true,
 })
 export class UnwrapPipe implements PipeTransform {
-  transform<T>(value: Result<T>, defaultValue: T): T {
-    return value.type === 'ok' ? value.content : defaultValue;
+  transform<T>(value: Nil<Result<T>>, defaultValue: T): T {
+    return value?.type === 'ok' ? value.content : defaultValue;
   }
 }
