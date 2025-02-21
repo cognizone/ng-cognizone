4d8109e863f994d61e6aa429427ba5cd9ab0fe4d
William Chelman <WilliamChelman@users.noreply.github.com>
2025-02-21 14:45:59 +0100

    fix(shacl-core): add getTargetClasses for consumers
    (cherry picked from commit 9ccf49c8ffcc51d2e69f974381b111a71812e2bc)

--- libs/shacl-core/src/lib/utils/index.ts
+++ libs/shacl-core/src/lib/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './get-concrete-node-kind-of-value';
 export * from './get-concrete-node-kinds-of-property-shape';
 export * from './get-path-descriptor';
+export * from './get-target-classes';
