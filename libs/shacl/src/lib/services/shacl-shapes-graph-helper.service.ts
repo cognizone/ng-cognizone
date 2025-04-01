dcbbcff13bfcab8e4b7e61a6fdb3bd4bfa2ecfb9
William Chelman <WilliamChelman@users.noreply.github.com>
2025-04-01 14:52:45 +0200

    fix(shacl): avoid duplicates in properties paths
    (cherry picked from commit dce87b562c6ee5b1ca8cd3860bb495ec05301512)
    (cherry picked from commit 2271ae2916404367a78467bf8de91fed6f5badbb)

--- libs/shacl/src/lib/services/shacl-shapes-graph-helper.service.ts
+++ libs/shacl/src/lib/services/shacl-shapes-graph-helper.service.ts
@@ -114,7 +114,7 @@ export class ShaclShapesGraphHelper {
   getPropertiesPathsOfNodeShape(nodeShape: ShNodeShape): string[] {
     return this.getAllPropertyShapeInNodeShape(nodeShape).reduce((acc, propertyShapeUri) => {
       const { path } = getPathDescriptor(propertyShapeUri, this.shapesGraph);
-      if (path) acc.push(path);
+      if (path && !acc.includes(path)) acc.push(path);
       return acc;
     }, [] as string[]);
   }
