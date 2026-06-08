af3ac3653c53cd7d3545060732d46ce81f02c6b7
William Chelman <WilliamChelman@users.noreply.github.com>
2026-06-08 13:39:27 +0200

    chore: update peer deps for 8.0.0

--- tools/set-peer-deps.js
+++ tools/set-peer-deps.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 // Hardcoded dependency version mappings
 const DEPENDENCY_VERSIONS = {
-  '@cognizone/': '>=8.0.0-beta.1',
+  '@cognizone/': '>=8.0.0',
   '@ngxs/': '>=21.0.0',
   '@angular/': '>=21.0.0',
   '@jsverse/': '>=8.0.0',
